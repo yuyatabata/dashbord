8d8583e0c2a66b3325919e62bc165cd39418b96d
田端佑也 <tabayu5@gmail.com>
2020-10-29 23:21:39 +0900

    start react

---
+++ chart_lesson/src/components/LinePlot.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import from "react-chartjs-2"
+
+const LinePlot : React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LinePlot
