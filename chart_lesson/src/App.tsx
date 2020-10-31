913798174e55201a16c79873c57e426d4780aa3e
田端佑也 <tabayu5@gmail.com>
2020-10-31 16:26:20 +0900

    done pie_plot

--- chart_lesson/src/App.tsx
+++ chart_lesson/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LinePlot from './components/LinePlot';
+import PiePlot from './components/PiePlot';
 
 function App() {
   return <div className="App">
     <LinePlot />
+    <PiePlot />
   </div>
 }
 
