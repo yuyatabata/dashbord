59ff94cb8f75da04cfb540ec71d99a502ee6db4c
田端佑也 <tabayu5@gmail.com>
2020-11-08 22:11:55 +0900

    add switch country

--- covid_dashbord/src/App.tsx
+++ covid_dashbord/src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Cards from "./features/covid/Cards/Cards";
 import Chart from "./features/covid/Chart/Chart";
 import PieChart from "./features/covid/PieChart/PieChart";
+import SwitchCountry from "./features/covid/SwitchCountry/SwitchCountry";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Cards />
         <Chart />
         <PieChart />
+        <SwitchCountry />
       </header>
     </div>
   );
