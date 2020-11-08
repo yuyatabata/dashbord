92fc8b1f16633995e18a8496b61f33a8baec960c
田端佑也 <tabayu5@gmail.com>
2020-11-08 16:14:38 +0900

    add piechart

--- covid_dashbord/src/App.tsx
+++ covid_dashbord/src/App.tsx
@@ -4,6 +4,7 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Cards from "./features/covid/Cards/Cards";
 import Chart from "./features/covid/Chart/Chart";
+import PieChart from "./features/covid/PieChart/PieChart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <header className="App-header">
         <Cards />
         <Chart />
+        <PieChart />
       </header>
     </div>
   );
